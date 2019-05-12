5b6a371093035da140426a6699643025c91157f2
Robert C Jensen <robert.cole.jensen@gmail.com>
2019-05-12 17:02:42 -0400

    add file admin

--- ermombs_admin/__init__.py
+++ ermombs_admin/__init__.py
@@ -1,18 +1,30 @@
 import os
 from flask import Flask
 from flask_admin import Admin
+from flask_admin.contrib.fileadmin import FileAdmin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
+
+# -----------------------------------------------------------------------------
+
+
 app = Flask(__name__)
+
 
 app.config['DATABASE_FILE'] = os.environ['DATABASE_FILE']
 app.config['FLASK_ADMIN_SWATCH'] = 'paper'
 app.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['STATIC_ROOT'] = os.environ['STATIC_ROOT']
+
+
+# -----------------------------------------------------------------------------
+
 
 db = SQLAlchemy(app)
+
 
 class Info(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,12 +33,14 @@ class Info(db.Model):
     featured_image_filename = db.Column(db.Text, nullable=False)
     featured_text = db.Column(db.Text, default='')
 
+
 class Series(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     featured_image_filename = db.Column(db.Text, nullable=False)
     featured_text = db.Column(db.Text, default='')
     slug = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
+
 
 class Work(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,14 +52,24 @@ class Work(db.Model):
     title = db.Column(db.Text, nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
+
+# -----------------------------------------------------------------------------
+
 InfoView = ModelView(Info, db.session)
 SeriesView = ModelView(Series, db.session)
 WorkView = ModelView(Work, db.session)
 
+
+# -----------------------------------------------------------------------------
+
+
 admin = Admin(name='ermombs.com admin', template_mode='bootstrap3')
 
+
+admin.add_view(FileAdmin(app.config['STATIC_ROOT'], '/static/', name='Static Files'))
 admin.add_view(InfoView)
 admin.add_view(SeriesView)
 admin.add_view(WorkView)
+
 
 admin.init_app(app)
